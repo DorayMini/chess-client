d3867211213d4929bddd0d0ae2db44d5ecde5b84
Hriptun Roman <hriptunroman602@gmail.com>
2026-06-10 11:47:14 +0300

    Delete client directory

--- client/js/config.js
+++
@@ -1,7 +0,0 @@
-const protocol = window.location.protocol; 
-const hostname = window.location.hostname; 
-const port = "8081"; 
-
-window.APP_CONFIG = {
-    API_URL: `${protocol}//${hostname}:${port}` 
-};
